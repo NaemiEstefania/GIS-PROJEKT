62b963f9201819cc34f9d8be8a0eb666706cf68e
Your Name <youremail@yourdomain.com>
2024-11-04 17:01:47 +0100

    anpassungen

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let element = document.getElementById("title");
-element.style.color = ("pink");
+element.addEventListener ("click", onclick); /* bei click wird es pink */
+function onclick  (event) {
+    element.style.color = ("pink");
+}
+element.value = "tipp here xd";
+ /*let element2 = document.querySelector(".book-form"); 
+element2.innerHTML = "<p>hallo</p>" /*beides zum hinzufügen 
+let newelement = document.createElement("p");
+newelement.textContent = "hiii";
+element2.append(newelement); */
