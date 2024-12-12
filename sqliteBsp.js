f826ddb4ef9c2e19b72db24f408cd4436d3a7fa5
Your Name <youremail@yourdomain.com>
2024-12-12 15:39:59 +0100

    sqlitebsp

---
+++ sqliteBsp.js
@@ -0,0 +1,24 @@
+// SQLite Modul in Node.js Code verwenden
+// vorlesung code node.js beispiel
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+
+// SQLite Datei angeben pfad angeben 
+const dbFilePath = 'hochschule.db';
+
+async function main() {
+  // Mit Datenbank verbinden
+  const db = await sqlite.open({
+    filename: dbFilePath,
+    driver: sqlite3.Database,
+  });
+
+  // Auf Datenbank zugreifen (z. B. SELECT Befehl)
+  const students = await db.all('SELECT * FROM student');
+  console.log(students);
+
+  // Verbindung zu Datenbank beenden 
+  await db.close();
+}
+
+main();
